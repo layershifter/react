0199d8c2999601274ee6f1722eb1c1e4ffecbb19
Silviu Avram <siavram@microsoft.com>
2018-08-15 11:04:27 +0200

    removed unnecessary changes

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -46,9 +46,6 @@ class Menu extends AutoControlledComponent<any, any> {
 
     /** Accessibility behavior if overridden by the user. */
     accessibility: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
-
-    /** Accessibility label that will be added by the accessibility behavior. */
-    'aria-label': PropTypes.string,
   }
 
   static defaultProps = {
@@ -68,7 +65,6 @@ class Menu extends AutoControlledComponent<any, any> {
     'shape',
     'type',
     'vertical',
-    'aria-label',
   ]
 
   static autoControlledProps = ['activeIndex']
